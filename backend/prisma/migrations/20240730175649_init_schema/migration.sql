a9bf868bedef039b913f0711c3796ede28dd0a6d
cyberrrnaut <cyberrrnaut@gmail.com>
2024-07-31 12:15:43 +0530

    31st jul

---
+++ backend/prisma/migrations/20240730175649_init_schema/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Blog" ADD CONSTRAINT "Blog_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,71 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
+import { decode, sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+type Bindings = {
+    DATABASE_URL:string,
+    JWT_SECRET:string
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+const JWT_SECRET="3725"
+
+app.post('/api/v1/signup', async(c) => {
+	const { DATABASE_URL } = c.env;
+
+  const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: DATABASE_URL, 
+      },
+    },
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+   const user=  await prisma.user.create({
+    data:{
+      email:body.email,
+      password: body.password
+    }
+   })
+
+  
+
+
+    
+    const token = await sign({id:user.id}, c.env.JWT_SECRET)
+
+
+  return c.json({
+    bearer: token
+  })
 })
+
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+	return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+
+
 
 export default app
 
